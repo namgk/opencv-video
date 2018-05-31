765b97ae1ab5a50ae9d3453ccb66e56e00ae6d7b
Nam Giang <kyng@ece.ubc.ca>
2018-05-31 13:49:37 -0700

    internalize caffe model

--- video3.py
+++ video3.py
@@ -7,12 +7,9 @@ import os
 import csv
 
 
-caffe_root = '/home/nhong/workspace/caffe/'  # this file should be run from {caffe_root}/examples (otherwise change this line)
-sys.path.insert(0, caffe_root + 'python')
-
 labels = list()
 
-with open('/home/nhong/stanford-cars/labels.csv', 'rb') as csvfile:
+with open('./stanford-cars-model/labels.csv', 'rb') as csvfile:
     labels_data = csv.reader(csvfile, delimiter=',')
     for row in labels_data:
         labels.append(row[1])
@@ -23,14 +20,14 @@ import caffe_pb2
 caffe.set_mode_cpu()
 
 mean_blob = caffe_pb2.BlobProto()
-with open(caffe_root + 'models/stanford-cars/mean.binaryproto') as f:
+with open('./stanford-cars-model/mean.binaryproto') as f:
     mean_blob.ParseFromString(f.read())
 mean_array = numpy.asarray(mean_blob.data, dtype=numpy.float32).reshape(
     (mean_blob.channels, mean_blob.height, mean_blob.width))
 
 
-model_def = caffe_root + 'models/stanford-cars/deploy.prototxt'
-model_weights = caffe_root + 'models/stanford-cars/caffe_model_1_iter_22169.caffemodel'
+model_def = './stanford-cars-model/deploy.prototxt'
+model_weights = './stanford-cars-model/caffe_model_1_iter_22169.caffemodel'
 
 net = caffe.Net(model_def,      # defines the structure of the model
                 model_weights,  # contains the trained weights
@@ -75,9 +72,3 @@ while True:
     output_prob = output['prob'][0]
 
     print 'predicted class is:', labels[output_prob.argmax()]
-
-    # load ImageNet labels
-    # labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt'
-    # labels = numpy.loadtxt(labels_file, str, delimiter='\t')
-
-    # print 'output label:', labels[output_prob.argmax()]
